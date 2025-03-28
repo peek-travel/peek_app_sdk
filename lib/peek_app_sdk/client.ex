0bd9f4d4b115731d14851f182ce879088e110bf9
Greg Coladarci <coladarci@gmail.com>
2025-03-27 20:14:11 -0700

    Magically determine operation name

--- lib/peek_app_sdk/client.ex
+++ lib/peek_app_sdk/client.ex
@@ -1,34 +1,64 @@
 defmodule PeekAppSDK.Client do
+  require Logger
+
   use Tesla
 
   plug Tesla.Middleware.JSON, engine_opts: [keys: &String.to_atom/1]
 
-  def query_peek_pro(install_id, gql_query, gql_variables \\ %{}, operation_name \\ nil) do
+  def query_peek_pro(install_id, gql_query, gql_variables \\ %{}) do
     body_params = %{
       "query" => gql_query,
       "variables" => gql_variables
     }
 
     peek_app_id = Application.fetch_env!(:peek_app_sdk, :peek_app_id)
-
+    operation_name = operation_name(gql_query)
     url = "#{api_url()}/#{peek_app_id}/#{operation_name}"
 
     case request(method: :post, url: url, body: body_params, headers: headers(install_id)) do
       {:ok, %Tesla.Env{status: 200, body: %{data: data}}} ->
         {:ok, data}
 
-      {:ok, %Tesla.Env{status: status}} ->
+      {:ok, %Tesla.Env{status: status, body: body}} ->
+        Logger.error("Unexpected PeekPro response (#{status}): #{inspect(body)}")
         {:error, status}
     end
   end
 
-  defp headers(install_id) do
-    peek_app_key = Application.fetch_env!(:peek_app_sdk, :peek_app_key)
-    token = PeekAppSDK.Token.new_for_app_installation!(install_id)
+  def operation_name(query) do
+    regex = ~r/\b(query|mutation|subscription)\s+(\w+)/
 
+    case Regex.run(regex, query) do
+      [_, _, operation_name] ->
+        operation_name
+
+      _ ->
+        raise """
+        Please include an operation name in all GraphQL queries/mutations.
+
+        bad:
+
+        query($id: ID!) {
+          booking(id: $id) {
+            displayId
+          }
+        }
+
+        good:
+
+        query GetBooking($id: ID!) {
+          booking(id: $id) {
+            displayId
+          }
+        }
+        """
+    end
+  end
+
+  defp headers(install_id) do
     [
-      {"X-Peek-Auth", "Bearer #{token}"},
-      {"pk-api-key", peek_app_key}
+      {"X-Peek-Auth", "Bearer #{PeekAppSDK.Token.new_for_app_installation!(install_id)}"},
+      {"pk-api-key", Application.fetch_env!(:peek_app_sdk, :peek_app_key)}
     ]
   end
 
