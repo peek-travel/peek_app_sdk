21b4315663b9813afa6590e483dc810b1bf68387
Martin <83237796+martInatpeek@users.noreply.github.com>
2025-04-07 18:01:27 -0400

    fix multi config header (#3)

--- test/peek_app_sdk/client_test.exs
+++ test/peek_app_sdk/client_test.exs
@@ -65,6 +65,43 @@ defmodule PeekAppSDK.ClientTest do
       assert {:ok, ^response_data} = Client.query_peek_pro(install_id, query, variables, :semnox)
     end
 
+    test "token is signed with the right peek_app_secret" do
+      install_id = "test_install_id"
+      query = "query Test { test }"
+      variables = %{"foo" => "bar"}
+      response_data = %{test: "success"}
+
+      expect(PeekAppSDK.MockTeslaClient, :call, fn env, _opts ->
+        assert env.method == :post
+
+        assert env.url ==
+                 "https://apps.peekapis.com/backoffice-gql/semnox_app_id/test"
+
+        assert Jason.decode!(env.body) == %{
+                 "query" => query,
+                 "variables" => variables
+               }
+
+        bearer_token =
+          env.headers
+          |> Enum.find(fn {k, _v} ->
+            k == "X-Peek-Auth"
+          end)
+          |> elem(1)
+
+        assert String.starts_with?(bearer_token, "Bearer ")
+        assert String.length(bearer_token) > 10
+
+        token = String.replace_prefix(bearer_token, "Bearer ", "")
+        assert {:ok, ^install_id, _claims} = PeekAppSDK.Token.verify_peek_auth(token, :semnox)
+        assert PeekAppSDK.Token.verify_peek_auth(token) == {:error, :unauthorized}
+
+        {:ok, %Tesla.Env{status: 200, body: %{data: response_data}}}
+      end)
+
+      assert {:ok, ^response_data} = Client.query_peek_pro(install_id, query, variables, :semnox)
+    end
+
     test "pk-api-key only sent if peek_app_key is set" do
       install_id = "test_install_id"
       query = "query Test { test }"
