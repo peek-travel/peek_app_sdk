661887967ed19d2253ea0c21d9064462f68dbb85
Martin <martin@peek.com>
2025-04-08 12:04:49 -1000

    fix for get_text

--- lib/peek_app_sdk/ui/get_text.ex
+++ lib/peek_app_sdk/ui/get_text.ex
@@ -5,7 +5,7 @@ defmodule PeekAppSDK.UI.Gettext do
   By using [Gettext](https://hexdocs.pm/gettext),
   your module gains a set of macros for translations, for example:
 
-      import AffiliateHubWeb.Gettext
+      import PeekAppSDK.UI.Gettext
 
       # Simple translation
       gettext("Here is the string to translate")
