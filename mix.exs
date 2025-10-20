73fa7a9c89765835c69c26a8b25c67b222d67ea7
Nicolas Falconi <nigfasa@gmail.com>
2025-10-20 16:29:45 -0300

    misc: add assets to mix file configuration

--- mix.exs
+++ mix.exs
@@ -94,7 +94,7 @@ defmodule PeekAppSDK.MixProject do
         "GitHub" => "https://github.com/peek-travel/peek_app_sdk"
       },
       files:
-        ~w(mix.exs lib/** package.json priv/static/odyssey_hooks.min.js priv/static/odyssey_web_components.min.js)
+        ~w(mix.exs lib/** assets/** package.json priv/static/odyssey_hooks.min.js priv/static/odyssey_web_components.min.js)
     ]
   end
 end
